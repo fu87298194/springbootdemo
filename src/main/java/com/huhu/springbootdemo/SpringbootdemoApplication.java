11700fc71c26373de793692a70a95c9dea399ec3
呼呼 <87298194@qq.com>
2023-09-02 10:53:37 +0800

    Changes

--- src/main/java/com/huhu/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/huhu/springbootdemo/SpringbootdemoApplication.java
@@ -3,13 +3,17 @@ package com.huhu.springbootdemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @MapperScan("com.huhu.springbootdemo.mapper")
 public class SpringbootdemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootdemoApplication.class, args);
+		ConfigurableApplicationContext  context=  SpringApplication.run(SpringbootdemoApplication.class, args);
+		Object redisTemplate = context.getBean("redisTemplate");
+		System.out.println(redisTemplate);
+
 	}
 
 }
--- src/test/java/com/huhu/springbootdemo/SpringbootdemoApplicationTests.java
+++ src/test/java/com/huhu/springbootdemo/SpringbootdemoApplicationTests.java
@@ -1,14 +1,35 @@
 package com.huhu.springbootdemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class SpringbootdemoApplicationTests {
+
+	@Autowired
+	private RedisTemplate redisTemplate;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void testSet(){
+		// 操作redis中的string类型的数据， 先获取ValueOperation对象
+		ValueOperations valueOperations = redisTemplate.opsForValue();
+		// 添加数据到redis
+		valueOperations.set("name", "zhangsan");
+
+
+	}
+
+	@Test
+	public void testGet() {
+		Object name = redisTemplate.boundValueOps("name").get();
+		System.out.println(name);
+	}
 
 }
